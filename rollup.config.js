e01d1f2ec8089ec8a56a0e85a2364e16e2f09061
mustofa-k <kamalmustofa241@gmail.com>
2024-07-10 15:44:23 +0700

    kamal4

--- rollup.config.js
+++ rollup.config.js
@@ -3,14 +3,9 @@ import { defineConfig } from "rollup";
 import image from "@rollup/plugin-image";
 
 export default defineConfig({
-  input: "src/main.js", // Sesuaikan dengan file masukan utama proyek Anda
-  output: {
-    file: "dist/bundle.js", // Sesuaikan dengan lokasi dan nama file keluaran bundel Anda
-    format: "es", // Format keluaran (misalnya, 'es' untuk modul ES6)
-  },
+  // Konfigurasi lainnya
   plugins: [
-    // Plugin Rollup yang Anda butuhkan
+    // Plugin lainnya
     image(),
-    // Plugin lainnya jika ada
   ],
 });
