ee7adf06120933dcb8ed65a1f7823eb9585504f9
mustofa-k <kamalmustofa241@gmail.com>
2024-07-12 15:37:39 +0700

    update sec projek

--- src/component/project/Projek.jsx
+++ src/component/project/Projek.jsx
@@ -1,30 +1,26 @@
-import imgprojek from '../assets/projek.jpg'; 
+import imgprojek from "../../assets/projek.jpg";
 import { Link } from "react-router-dom";
-
+import "./projek.css";
 function Project() {
   return (
     <>
-      <div className="container-fluid event py-5">
+      <div className="container-fluid event py-5 projek">
         <div className="container py-5">
           <h1 className="display-6 mb-5 wow fadeIn" data-wow-delay="0.1s">
-         <span className="text-span">Project</span>
+            <span className="text-span">Project</span>
           </h1>
           <div className="row g-4 event-item wow fadeIn" data-wow-delay="0.1s">
             <div className="col-3 col-lg-2 pe-0">
               <div className="text-center border-bottom border-dark py-3 px-2">
-                <h6>01 Jan 2045</h6>
-                <p className="mb-0">Fri 06:55</p>
+                <h6>Tugas Kuliah</h6>
               </div>
             </div>
             <div className="col-9 col-lg-6 border-start border-dark pb-5">
               <div className="ms-3">
-                <h4 className="mb-3">UI/UX Design For The Future</h4>
-                <p className="mb-4">
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-                  consequat. Duis aute irure dolor in
-                </p>
-                <Link to={'/'} className="btn btn-danger px-3">
-                  Join Now
+                <h4 className="mb-3">Membuat website CRUD</h4>
+                <p className="mb-4">Pada waktu semester, saya mengerjakan tugas membuat website dengan fitur CRUD menggunakan HTML, CSS, dan PHP, serta menggunakan Bootstrap untuk database.</p>
+                <Link to={"/"} className="btn btn-danger px-3">
+                  Github
                 </Link>
               </div>
             </div>
@@ -37,19 +33,18 @@ function Project() {
           <div className="row g-4 event-item wow fadeIn" data-wow-delay="0.3s">
             <div className="col-3 col-lg-2 pe-0">
               <div className="text-center border-bottom border-dark py-3 px-2">
-                <h6>01 Jan 2045</h6>
-                <p className="mb-0">Wed 11:30</p>
+                <h6>Tugas akhir Bootcamp</h6>
               </div>
             </div>
             <div className="col-9 col-lg-6 border-start border-dark pb-5">
               <div className="ms-3">
-                <h4 className="mb-3">Blockchain</h4>
+                <h4 className="mb-3">Membuat Website course online</h4>
                 <p className="mb-4">
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-                  consequat. Duis aute irure dolor in
+                  Membuat UI untuk website kursus online menggunakan React.js dan Bootstrap, serta mengintegrasikan API dari tim backend. Fitur-fitur yang tersedia meliputi login, registrasi, kursus, pembelian kursus, filter kursus,
+                  pencarian kursus, riwayat pembelian kursus, dan dashboard admin untuk mengelola kursus.
                 </p>
-                <Link to={'/'} className="btn btn-danger px-3">
-                  Join Now
+                <Link to={"/"} className="btn btn-danger px-3">
+                  Github
                 </Link>
               </div>
             </div>
@@ -62,19 +57,15 @@ function Project() {
           <div className="row g-4 event-item wow fadeIn" data-wow-delay="0.5s">
             <div className="col-3 col-lg-2 pe-0">
               <div className="text-center border-bottom border-dark py-3 px-2">
-                <h6>01 Jan 2045</h6>
-                <p className="mb-0">Thu 11:30</p>
+                <h6>Latihan Membuat API</h6>
               </div>
             </div>
             <div className="col-9 col-lg-6 border-start border-dark pb-5">
               <div className="ms-3">
-                <h4 className="mb-3">Cloud Computing</h4>
-                <p className="mb-4">
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-                  consequat. Duis aute irure dolor in
-                </p>
-                <Link to={'/'} className="btn btn-danger px-3">
-                  Join Now
+                <h4 className="mb-3">Membuat API dengan express js</h4>
+                <p className="mb-4">Latihan membuat API menggunakan Express.js dan phpMyAdmin sebagai database, dengan fitur CRUD.</p>
+                <Link to={"/"} className="btn btn-danger px-3">
+                  Github
                 </Link>
               </div>
             </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,15 @@
 import Hero from "../component/HeroSection";
 import NavigationBars from "../component/Navbar";
-import Project from "../component/Projek";
+import Project from "../component/project/Projek";
 
 function HomePage() {
-    return ( 
-        <>
-        <NavigationBars/>
-        |<Hero/>
-        <Project/>
-        </>
-     );
+  return (
+    <>
+      <NavigationBars />
+      |<Hero />
+      <Project />
+    </>
+  );
 }
 
 export default HomePage;
